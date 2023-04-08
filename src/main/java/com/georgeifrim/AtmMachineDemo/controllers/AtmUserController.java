f421d59ae92bce3d4cd11501672ebc4ada5c90e6
GeorgeIfrim <georgealexandruifrim@gmail.com>
2023-04-08 17:23:38 +0300

    new app

--- src/main/java/com/georgeifrim/AtmMachineDemo/controllers/AtmUserController.java
+++ src/main/java/com/georgeifrim/AtmMachineDemo/controllers/AtmUserController.java
@@ -12,7 +12,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/user")
 public class AtmUserController {
-
+    //new AtmMachine Demo app
     private final AtmService atmService;
 
     @Autowired
